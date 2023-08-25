4f315314ab234f26e2ed33637c66c32c3b8ed775
corbinCD <collinscd@s.dcsdk12.org>
2023-08-25 14:55:14 -0600

    classes and objects

--- classes and objects.dart
+++ classes and objects.dart
@@ -1,4 +1,31 @@
 //page 24/37
+//also constructor and get and set
+// var automaticly public unless name starts with _ then its private
+//setter(set) updates its value getter(get) returns a property value
 void main(){
-  
+  //creates a constructor
+  Person clark = new Person('Clark', 'Kent');
+  clark.age = 100;
+  print('${clark.name} ${clark.surname} ${clark.age}');
+}
+
+class Person {
+  String name = "", surname = '';
+  int _age = 0;
+ Person(String name, String surname){
+  this.name = name;
+  this.surname = surname;
+ }
+  void set age(int years){
+    if(years > 0 && years < 120 ){
+      _age = years;
+    }
+    else {
+      _age = 0;
+    }
+  }
+
+  int get age {
+    return _age;
+  }
 }
